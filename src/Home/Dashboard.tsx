fdb3523472d6afd73de49ee8901699ec8c35400e
Việt Vui Vẻ <80948525+19522515@users.noreply.github.com>
2022-12-21 08:04:08 +0700

    Update Dashboard.tsx

--- src/Home/Dashboard.tsx
+++ src/Home/Dashboard.tsx
@@ -135,7 +135,7 @@ function Dashboard() {
   return (
     <div className="app">
       <div className="app-nav">
-        <h1>Trello Board</h1>
+        <h1>KanBan Board</h1>
       </div>
       <div className="app-boards-container">
         <div className="app-boards">
